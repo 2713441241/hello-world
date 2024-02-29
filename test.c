248224a3c16db615c6eda82b0084d98bae48fadd
wanjinyao <2713441241@qq.com>
2024-02-29 21:11:43 +0800

    test.c  V1.0

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	printf("short:%d\n",sizeof(short));
+	printf("unsgned short:%lu\n", sizeof(unsigned short));
+	printf("int:%d\n", sizeof(int));
+	printf("unsigned int:%d\n", sizeof(unsigned int));
+	printf("long:%d\n", sizeof(long));
+	printf("unsigned long:%d\n", sizeof(unsigned long));
+	printf("long long:%d\n", sizeof(long long));
+	printf("unsigned long long:%d\n", sizeof(unsigned long long));
+	printf("float:%d\n", sizeof(float));
+	printf("double:%d\n", sizeof(double));
+	printf("char:%d\n", sizeof(char));
+ 
+	return 0;
+}
+
+
+
+
+
